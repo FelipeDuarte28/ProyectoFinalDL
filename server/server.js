67a12e5cea068c082d465c60ccef253a54010618
FelipeDuarte28 <114889470+FelipeDuarte28@users.noreply.github.com>
2023-08-27 18:31:06 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     connectionString: process.env.DB_URL,
