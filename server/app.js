71ab18d56e2bd947609661769ca228d924f4dad9
FelipeDuarte28 <114889470+FelipeDuarte28@users.noreply.github.com>
2023-08-27 20:24:32 -0400

    Update app.js cambio en cors

--- server/app.js
+++ server/app.js
@@ -5,13 +5,12 @@ const router = require("./router/routers.js")
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors());
 
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-});
+app.use(cors({
+    origin: "https://bicimarketplacefront.onrender.com",
+    headers: ["Content-Type"],
+    credentials: true,
+}));
 
 app.get("/", (req, res) => {
     res.send("Hola desde node")
