105bdaf7175dc7c51a2aaf4ca473ac7f7ffe2b21
FelipeDuarte28 <114889470+FelipeDuarte28@users.noreply.github.com>
2023-08-27 22:33:21 -0400

    Update routers.js

--- server/router/routers.js
+++ server/router/routers.js
@@ -7,6 +7,7 @@ const { getProducts, postProducts, getReviews,
     setFavoritos, getUser, deleteFavoritos,
     getUserProducts, getEditProduct } = require("../consultas");
 const checkCredentiaslMiddleware = require("../middlewares/middlewares");
+const secretkey = process.env.SECRET_KEY;
 require("dotenv").config();
 
 // Middleware
@@ -70,7 +71,7 @@ router.post("/login", checkCredentiaslMiddleware, async (req, res) => {
         const user = req.body;
         const { email, password } = user;
         await userLogin(email, password);
-        const token = jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: "1h" });
+        const token = jwt.sign({ email }, secretkey, { expiresIn: "1h" });
         res.send(token);
     } catch (error) {
         res.status(error.code || 500).send({ message: "Error during login", details: error.message });
